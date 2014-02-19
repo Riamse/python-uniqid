196e6a12f7ab09d4adf7820ffe86547a601c4c7c
riamse2 <riamse@protonmail.com>
2014-02-19 15:13:07 -0800

    Stupid readme

--- setup.py
+++ setup.py
@@ -1,8 +1,30 @@
-from setuptools import *
+#!/usr/bin/python3
+
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    from distutils.core import setup, Extension
+
+def read_long_description():
+    try:
+        return open("README.md").read()
+    except Exception:
+        return ''
 
 mod = Extension("uniqid", sources=["uniqidmodule.c"])
 
 setup(name="uniqid",
+      version='0.0.1',
       ext_modules=[mod],
+      url='https://github.com/riamse2/python-uniqid',
+      license="PHP License version 3.01",
+      author="Stir Sæther Bakken",
+      author_email='ssb@php.net',
+      keywords='uniqid php',
+      description="Generate a unique ID, à la PHP",
+      long_description=read_long_description(),
+      classifiers=["Intended Audience :: Developers",
+                   "Programming Language :: Python :: 3",
+      ],
 )
 
