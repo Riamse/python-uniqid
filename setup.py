15b5145709369b5547518567cf89685be9b1b475
derwentx <derwentx@gmail.com>
2016-02-23 13:56:22 +0800

    made compatible with python2
    ported uniqidmodule.c to uniqid.py and changed setuptools.setup()
    arguments if in python2

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+# -*- coding: utf8 -*-
+
+import sys
 
 try:
     from setuptools import setup, Extension
@@ -11,20 +14,25 @@ def read_long_description():
     except Exception:
         return ''
 
-mod = Extension("uniqid", sources=["uniqidmodule.c"])
+setup_arguments = {
+  'name':"uniqid",
+  'version':'0.0.2',
+  'url':'https://github.com/Riamse/python-uniqid',
+  'license':"PHP License version 3.01",
+  'author':"Stig Sæther Bakken",
+  'author_email':'ssb@php.net',
+  'keywords':'uniqid php',
+  'description':"Generate a unique ID, à la PHP",
+  'long_description':read_long_description(),
+  'classifiers':["Intended Audience :: Developers",
+               "Programming Language :: Python :: 3",
+  ],
+}
 
-setup(name="uniqid",
-      version='0.0.1',
-      ext_modules=[mod],
-      url='https://github.com/Riamse/python-uniqid',
-      license="PHP License version 3.01",
-      author="Stig Sæther Bakken",
-      author_email='ssb@php.net',
-      keywords='uniqid php',
-      description="Generate a unique ID, à la PHP",
-      long_description=read_long_description(),
-      classifiers=["Intended Audience :: Developers",
-                   "Programming Language :: Python :: 3",
-      ],
-)
-
+if sys.version_info[0] >= 3:
+  mod = Extension("uniqid", sources=["uniqidmodule.c"])
+  setup_arguments['ext_modules'] = [mod]
+  setup(**setup_arguments)
+else:
+  setup_arguments['py_modules'] = ['uniqid']
+  setup(**setup_arguments)
