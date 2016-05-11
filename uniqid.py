033b98225e48b4973db0aee96644dda900a25e49
riamse2 <riamse@protonmail.com>
2016-05-10 17:34:14 -0700

    Minor stylistic changes and things

--- uniqid.py
+++ uniqid.py
@@ -4,14 +4,32 @@ import time, math, random
 # inspired by http://gurukhalsa.me/2011/uniqid-in-python/
 
 def uniqid(prefix='', more_entropy=False):
+    """uniqid([prefix=''[, more_entropy=False]]) -> str
+    Gets a prefixed unique identifier based on the current
+    time in microseconds.
+
+    prefix
+        Can be useful, for instance, if you generate identifiers
+        simultaneously on several hosts that might happen to generate
+        the identifier at the same microsecond.
+        With an empty prefix, the returned string will be 13 characters
+        long. If more_entropy is True, it will be 23 characters.
+
+    more_entropy
+        If set to True, uniqid() will add additional entropy (using
+        the combined linear congruential generator) at the end of
+        the return value, which increases the likelihood that
+        the result will be unique.
+
+    Returns the unique identifier, as a string."""
     m = time.time()
     sec = math.floor(m)
     usec = math.floor(1000000 * (m - sec))
     if more_entropy:
         lcg = random.random()
-        uniqid = "%08x%05x%.8F" % (sec, usec, lcg*10)
+        the_uniqid = "%08x%05x%.8F" % (sec, usec, lcg * 10)
     else:
-        uniqid = '%8x%05x' %(sec, usec)
+        the_uniqid = '%8x%05x' % (sec, usec)
 
-    uniqid = prefix + uniqid
-    return uniqid
+    the_uniqid = prefix + the_uniqid
+    return the_uniqid
